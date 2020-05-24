0749113699124bc2c1b4347b884445dc074aaf83
Mark Carver <mark.carver@me.com>
2020-05-24 13:57:17 -0500

    Issue #3142419 by markcarver: Support new bundled Footnotes extension
    Signed-off-by: Mark Carver <mark.carver@me.com>

--- src/Plugin/Markdown/CommonMark/Extension/FootnoteExtension.php
+++ src/Plugin/Markdown/CommonMark/Extension/FootnoteExtension.php
@@ -8,35 +8,54 @@ use Drupal\Core\Theme\ActiveTheme;
 use Drupal\markdown\Plugin\Markdown\AllowedHtmlInterface;
 use Drupal\markdown\Plugin\Markdown\CommonMark\BaseExtension;
 use Drupal\markdown\Plugin\Markdown\ParserInterface;
+use Drupal\markdown\Plugin\Markdown\SettingsInterface;
+use Drupal\markdown\Traits\SettingsTrait;
 use League\CommonMark\ConfigurableEnvironmentInterface;
-use RZ\CommonMark\Ext\Footnote\FootnoteExtension as RZFootnoteExtension;
 
 /**
  * Footnotes extension.
  *
  * @MarkdownExtension(
- *   id = "rezozero/commonmark-ext-footnotes",
+ *   id = "league/commonmark-ext-footnotes",
  *   label = @Translation("Footnotes"),
- *   installed = "\RZ\CommonMark\Ext\Footnote\FootnoteExtension",
  *   description = @Translation("Adds the ability to create footnotes in markdown."),
- *   url = "https://github.com/rezozero/commonmark-ext-footnotes",
+ *   installed = {
+ *     "\League\CommonMark\Extension\Footnote\FootnoteExtension" = {
+ *       "url" = "https://commonmark.thephpleague.com/extensions/footnotes/",
+ *       "version" = "\League\CommonMark\CommonMarkConverter::VERSION",
+ *       "versionConstraint" = "^1.5 || ^2.0",
+ *     },
+ *     "\RZ\CommonMark\Ext\Footnote\FootnoteExtension" = {
+ *       "deprecated" = @Translation("Support for this extension was deprecated in markdown:8.x-2.0 and will be removed from markdown:3.0.0."),
+ *       "url" = "https://github.com/rezozero/commonmark-ext-footnotes",
+ *     },
+ *   },
  * )
  * @MarkdownAllowedHtml(
- *   id = "rezozero/commonmark-ext-footnotes",
+ *   id = "league/commonmark-ext-footnotes",
  *   label = @Translation("Footnotes"),
- *   installed = "\RZ\CommonMark\Ext\Footnote\FootnoteExtension",
+ *   installed = {
+ *     "\League\CommonMark\Extension\Footnote\FootnoteExtension" = {
+ *       "url" = "https://commonmark.thephpleague.com/extensions/footnotes/",
+ *       "version" = "\League\CommonMark\CommonMarkConverter::VERSION",
+ *       "versionConstraint" = "^1.5 || ^2.0",
+ *     },
+ *     "\RZ\CommonMark\Ext\Footnote\FootnoteExtension" = {
+ *       "deprecated" = @Translation("in markdown:8.x-2.0 and is removed from markdown:3.0.0. Use https://commonmark.thephpleague.com/extensions/footnotes/ instead."),
+ *       "url" = "https://github.com/rezozero/commonmark-ext-footnotes",
+ *     },
+ *   },
  * )
- *
- * @todo Add settings if they ever become configurable.
- * @see https://github.com/rezozero/commonmark-ext-footnotes/issues/7
  */
-class FootnoteExtension extends BaseExtension implements AllowedHtmlInterface, PluginFormInterface {
+class FootnoteExtension extends BaseExtension implements AllowedHtmlInterface, PluginFormInterface, SettingsInterface {
+
+  use SettingsTrait;
 
   /**
    * {@inheritdoc}
    */
   public function allowedHtmlTags(ParserInterface $parser, ActiveTheme $activeTheme = NULL) {
-    return [
+    $tags = [
       'a' => [
         'href' => TRUE,
         'role' => TRUE,
@@ -47,7 +66,6 @@ class FootnoteExtension extends BaseExtension implements AllowedHtmlInterface, P
         'id' => TRUE,
         'role' => TRUE,
       ],
-      'hr' => [],
       'li' => [
         'class' => TRUE,
         'id' => TRUE,
@@ -57,6 +75,39 @@ class FootnoteExtension extends BaseExtension implements AllowedHtmlInterface, P
       'sup' => [
         'id' => TRUE,
       ],
+    ];
+
+    if (!$this->isPreferredInstall() || $parser->extension($this->pluginId)->getSetting('container_add_hr')) {
+      $tags['hr'] = [];
+    }
+
+    return $tags;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public static function defaultSettings(array $pluginDefinition) {
+    // Immediately return if not using the newer bundled extension.
+    if ($pluginDefinition['installedClass'] === 'RZ\\CommonMark\\Ext\\Footnote\\FootnoteExtension') {
+      return [];
+    }
+    return [
+      'backref_class'      => 'footnote-backref',
+      'container_add_hr'   => TRUE,
+      'container_class'    => 'footnotes',
+      'footnote_class'     => 'footnote',
+      // CommonMark defaults to using a colon delimiter ("fn:"), however
+      // this causes core's XSS filter to strip everything past it; use a
+      // hyphen (-) instead.
+      // @see https://www.drupal.org/project/drupal/issues/2544110
+      'footnote_id_prefix' => 'fn-',
+      'ref_class'          => 'footnote-ref',
+      // CommonMark defaults to using a colon delimiter ("fnref:"), however
+      // this causes core's XSS filter to strip everything past it; use a
+      // hyphen (-) instead.
+      // @see https://www.drupal.org/project/drupal/issues/2544110
+      'ref_id_prefix'      => 'fnref-',
     ];
   }
 
@@ -69,11 +120,64 @@ class FootnoteExtension extends BaseExtension implements AllowedHtmlInterface, P
     // Add a note about core's aggressive XSS and how it affects footnotes.
     // @todo Remove note about core XSS bug/workaround.
     // @see https://www.drupal.org/project/markdown/issues/3136378
-    $parent = &$form_state->getParentForm();
-    $parent['#description'] = $this->t('NOTE: There is bug that prevents the footnote identifiers from being rendered properly due to aggressive XSS filtering. There is a <a href=":issue" target="_blank">temporary workaround</a>, but you must manually implement it in a custom module.', [
-      ':issue' => 'https://www.drupal.org/project/markdown/issues/3131224#comment-13613381',
-    ]);
-    $parent['#description_display'] = 'before';
+    if (!$this->isPreferredInstall()) {
+      $parent = &$form_state->getParentForm();
+      $parent['xss_bug'] = [
+        '#weight' => -9,
+        '#theme' => 'status_messages',
+        '#message_list' => ['warning' => [
+          $this->t('There is a known bug that prevents footnote identifiers from being rendered properly due to aggressive XSS filtering. There is a <a href=":issue" target="_blank">temporary workaround</a>, but you must manually implement it in a custom module. It is highly recommended that you instead upgrade to a newer version of CommonMark (1.5+) which includes a new bundled Footnotes extension where these settings are customizable.', [
+            ':issue' => 'https://www.drupal.org/project/markdown/issues/3131224#comment-13613381',
+          ]),
+        ]],
+        '#status_headings' => [
+          'warning' => $this->t('Warning message'),
+        ],
+      ];
+      return $element;
+    }
+
+    $element += $this->createSettingElement('backref_class', [
+      '#type' => 'textfield',
+      '#title' => $this->t('Back Reference Class'),
+      '#description' => $this->t('Defines which HTML class should be assigned to rendered footnote back reference elements.'),
+    ], $form_state);
+
+    $element += $this->createSettingElement('container_add_hr', [
+      '#type' => 'checkbox',
+      '#title' => $this->t('Container Add Horizontal Rule'),
+      '#description' => $this->t('Controls whether an <code>&lt;hr&gt;</code> element should be added inside the container.  Disable this if you want more control over how the footnote section at the bottom is differentiated from the rest of the document.'),
+    ], $form_state);
+
+    $element += $this->createSettingElement('container_class', [
+      '#type' => 'textfield',
+      '#title' => $this->t('Container Class'),
+      '#description' => $this->t('Defines which HTML class should be assigned to the container at the bottom of the page which shows all the footnotes.'),
+    ], $form_state);
+
+    $element += $this->createSettingElement('footnote_class', [
+      '#type' => 'textfield',
+      '#title' => $this->t('Footnote Class'),
+      '#description' => $this->t('Defines which HTML class should be assigned to rendered footnote elements.'),
+    ], $form_state);
+
+    $element += $this->createSettingElement('footnote_id_prefix', [
+      '#type' => 'textfield',
+      '#title' => $this->t('Footnote ID Prefix'),
+      '#description' => $this->moreInfo($this->t('Defines the prefix prepended to footnote elements. Note: due to a core bug, the use of colons (:) is not possible.'), 'https://www.drupal.org/project/drupal/issues/2544110'),
+    ], $form_state);
+
+    $element += $this->createSettingElement('ref_class', [
+      '#type' => 'textfield',
+      '#title' => $this->t('Reference Class'),
+      '#description' => $this->t('Defines which HTML class should be assigned to rendered footnote reference elements.'),
+    ], $form_state);
+
+    $element += $this->createSettingElement('ref_id_prefix', [
+      '#type' => 'textfield',
+      '#title' => $this->t('Reference ID Prefix'),
+      '#description' => $this->moreInfo($this->t('Defines the prefix prepended to footnote references. Note: due to a core bug, the use of colons (:) is not possible.'), 'https://www.drupal.org/project/drupal/issues/2544110'),
+    ], $form_state);
 
     return $element;
   }
@@ -82,21 +186,28 @@ class FootnoteExtension extends BaseExtension implements AllowedHtmlInterface, P
    * {@inheritdoc}
    */
   public function register(ConfigurableEnvironmentInterface $environment) {
-    $environment->addExtension(new RZFootnoteExtension());
+    $environment->addExtension($this->instantiateInstalledClass());
   }
 
   /**
    * {@inheritdoc}
    */
-  public function submitConfigurationForm(array &$form, FormStateInterface $form_state) {
-    // Intentionally do nothing; implementation is required.
+  public function settingsKey() {
+    return $this->isPreferredInstall() ? 'footnote' : FALSE;
   }
 
   /**
    * {@inheritdoc}
    */
   public function validateConfigurationForm(array &$form, FormStateInterface $form_state) {
-    // Intentionally do nothing; implementation is required.
+    /** @var \Drupal\markdown\Form\SubformStateInterface $form_state */
+    foreach (['footnote_id_prefix', 'ref_id_prefix'] as $name) {
+      if (strpos($form_state->getValue($name), ':') !== FALSE) {
+        $form_state->setError($form[$name], $this->moreInfo($this->t('Due to a core bug, the use of colons (:) in "@title" is not possible.', [
+          '@title' => isset($form[$name]['#title']) ? $form[$name]['#title'] : $name,
+        ]), 'https://www.drupal.org/project/drupal/issues/2544110'));
+      }
+    }
   }
 
 }
